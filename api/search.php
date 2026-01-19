e64a25b156eafb565c0d210b5eb7ff14f382b2c3
Senechal jf <jfsenechal@gmail.com>
2026-01-19 14:09:13 +0100

    Add search endpoint integrating MeiliSearch with error handling

---
+++ api/search.php
@@ -0,0 +1,26 @@
+<?php
+
+use AcMarche\Theme\Lib\Mailer;
+use AcMarche\Theme\Lib\Search\MeiliSearch;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+require_once(__DIR__.'/../wp-load.php');
+$searcher = new MeiliSearch();
+$searcher->initClientAndIndex();
+$keyword = $_GET['s'] ?? null;
+if (!$keyword) {
+    $response = new JsonResponse(['error' => 'no keyword'], 500);
+    $response->send();
+}
+try {
+    $searching = $searcher->doSearch($keyword);
+    $hits = $searching->getHits();
+    $count = $searching->count();
+    $response = new JsonResponse($hits);
+    $response->send();
+} catch (Exception $e) {
+    Mailer::sendError("wp error search", $e->getMessage());
+    $hits = ['error' => $e->getMessage()];
+    $response = new JsonResponse($hits, 500);
+    $response->send();
+}
